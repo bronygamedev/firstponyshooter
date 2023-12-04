f6ac22b8d80a8f597fb5a6532e5e500b99134c34
malakai <arandombrony123+git@gmail.com>
2023-12-04 17:13:55 -0600

    Add settings menu and debug panel
    - Implemented a settings menu for user preferences
    - Added a debug panel for real-time performance metrics
    - Made minor tweaks to the environment for optimization

--- client/Scripts/HUD.gd
+++ client/Scripts/HUD.gd
@@ -1,7 +1,17 @@
 extends Control
 
 @onready var globalPlayerVars = get_node("/root/PlayerGlobalVarables")
+var settings = 1
+func _input(event):
+	if event.is_action_pressed("menu"):
+		$SettingsContainer.visible = bool(settings)
+		settings = not settings
+		if !settings:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+		
 
 func _ready():
 	$HealthBar.max_value = globalPlayerVars.maxhealth
